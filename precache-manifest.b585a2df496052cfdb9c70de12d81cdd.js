ea9aaaf807f8289910f89146e8a4660f24b7d1cf
Noah Sylwester <nanoabram@comcast.net>
2020-06-18 00:59:00 -0700

    Updates

---
+++ precache-manifest.b585a2df496052cfdb9c70de12d81cdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098b757d2292edd65762c5eaf28df403",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eb8317fcde02c0dceb2",
+    "url": "/static/css/main.78029cd6.chunk.css"
+  },
+  {
+    "revision": "52009aa6956bd76399c1",
+    "url": "/static/js/2.2f773e5e.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/static/js/2.2f773e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb8317fcde02c0dceb2",
+    "url": "/static/js/main.7101f70d.chunk.js"
+  },
+  {
+    "revision": "3ac27118c52e75d53249",
+    "url": "/static/js/runtime-main.4c2bc187.js"
+  },
+  {
+    "revision": "d3add68fce6310ab87609b6fe69429a5",
+    "url": "/static/media/splash.d3add68f.jpg"
+  }
+]);

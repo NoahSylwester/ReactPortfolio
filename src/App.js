0d4801db818684786bfce5bbe822f0a00229fe62
Noah Sylwester <noahsylwester@gmail.com>
2025-02-02 20:23:40 -0800

    add segno privacy policy

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 import Splash from './pages/Splash';
 import NewAbout from './pages/NewAbout';
 import NewPortfolio from './pages/NewPortfolio';
+import PrivacyPolicySegno from './pages/PrivacyPolicySegno';
 
 const routes = [
   { path: '/', name: 'Splash', Component: Splash },
   { path: '/about', name: 'About', Component: NewAbout },
   { path: '/portfolio', name: 'Portfolio', Component: NewPortfolio },
+  { path: '/segno-privacy', name: 'Privacy Policy', Component: PrivacyPolicySegno },
 ]
 
 export default function App() {
@@ -19,6 +21,7 @@ export default function App() {
         <Route exact path="/" component={Splash} />
         <Route exact path={"/about"} component={NewAbout} />
         <Route exact path={"/portfolio"} component={NewPortfolio} />
+        <Route exact path={"/segno-privacy"} component={PrivacyPolicySegno} />
       </div>
     </Router>
   )
